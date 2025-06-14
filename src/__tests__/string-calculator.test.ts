62c5033dd8671b8542d3b861c7d7d54de9f51f35
Rehan Mohiuddin <rehan.4942@gmail.com>
2025-06-14 16:29:05 +0530

    impl: string-calculator - support of comman seperated numnber

--- src/__tests__/string-calculator.test.ts
+++ src/__tests__/string-calculator.test.ts
@@ -11,3 +11,8 @@ test('returns the number itself if only one number is provided', () => {
 test('returns sum of two comma-separated numbers', () => {
     expect(add("1,2")).toBe(3);
 });
+
+test('returns sum of multiple comma-separated numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+  
--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,7 +1,8 @@
 export const add = (numbers: string) => {
     if (numbers === "") return 0;
 
-    const parts = numbers.split(",");
-
-    return parts.reduce((sum, num) => sum + parseInt(num), 0);
+    return numbers
+        .split(",")
+        .map(n => parseInt(n))
+        .reduce((sum, n) => sum + (isNaN(n) ? 0 : n), 0);
 }
