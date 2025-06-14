53cca3366e5f2a50b062c3eea7e63b4ab50d5543
Rehan Mohiuddin <rehan.4942@gmail.com>
2025-06-14 16:31:06 +0530

    impl: string-calculator - support newline (\n) as delimiters

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,8 +1,8 @@
 export const add = (numbers: string) => {
     if (numbers === "") return 0;
 
-    return numbers
-        .split(",")
-        .map(n => parseInt(n))
-        .reduce((sum, n) => sum + (isNaN(n) ? 0 : n), 0);
+
+    const parts = numbers.split(/[\n,]/);
+
+    return parts.reduce((sum, num) => sum + parseInt(num), 0);
 }
