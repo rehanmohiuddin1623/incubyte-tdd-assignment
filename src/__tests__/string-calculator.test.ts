8764350bcac46ed75c3fc37cea86264dc31dfd48
Rehan Mohiuddin <rehan.4942@gmail.com>
2025-06-14 16:25:27 +0530

    test: string-calculator - return sum of comma seprated numbers

--- src/__tests__/string-calculator.test.ts
+++ src/__tests__/string-calculator.test.ts
@@ -1,10 +1,13 @@
 import { add } from '../index';
 
 test('returns 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
 });
 
 test('returns the number itself if only one number is provided', () => {
     expect(add("1")).toBe(1);
-  });
-  
+});
+
+test('returns sum of two comma-separated numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
